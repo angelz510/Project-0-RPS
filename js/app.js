7caa556561aa20f3e2252046ce99d628882d3100
angelz510 <angelzrod4@gmail.com>
2020-02-13 11:18:39 -0800

    added scoreboard function to reflec player and computer points

--- js/app.js
+++ js/app.js
@@ -11,27 +11,26 @@ reset.addEventListener("click", function() {
 
 //Scores
 let playerPoints = 0;
-let playerScore = document.getElementById("player-score");
 let computerPoints = 0;
+let playerScore = document.getElementById("player-score");
 let computerScore = document.getElementById("computer-score");
 
 // Defining rock
 let rock = document.getElementById("rock");
 rock.addEventListener("click", function(event){
-    playerPick(event.target.classname);
+    playerPick(event.target.className);
 })
     
-
 // Defining paper
 let paper = document.getElementById("paper");
 paper.addEventListener("click", function(event){
-    playerPick(event.target.classname);
+    playerPick(event.target.className);
 })
 
 // Defining scissors
 let scissors = document.getElementById("scissors");
 scissors.addEventListener("click", function(event){
-    playerPick(event.target.classname);
+    playerPick(event.target.className);
 })
 
 // Random computer pick
@@ -43,34 +42,66 @@ function computerPick() {
     return picks[randomPick];
 }
 
+
 // Player pick
 function playerPick(choice) {
     let computerPicks = computerPick();
-    let picks = ["rock", "paper", "scissors"];
+    // Winning Combos
     if (choice == "rock" && computerPicks == "paper") {
-        console.log('You lose!');
-    } else if (computerPicks == "scissors") {
-        console.log('You win!'); 
-    } else if (computerPicks == "rock") {
-        console.log('DRAW');
+        console.log('You picked Rock and CPU picked Paper: You Lose IDK how'); 
+        // computer ++
+        computerPoints++;
+    }   if (choice == "rock" && computerPicks == "scissors") {
+        console.log('You picked Rock and CPU picked Scissors: You Win'); 
+        // player ++
+        playerPoints++;
+    }   if (choice == "rock" && computerPicks == "rock") {
+        console.log('You picked Rock and CPU picked Rock: DRAW');
     } 
-    if (choice == "paper" && computerPick == "rock") {
-        console.log('You Win!');
-    } else if (computerPick == "paper") {
-        console.log('DRAW');
-    } else if (computerPick == "scissors") {
-        console.log('You lose!');
-    }
-    if (choice == "scissors" && computerPick == "rock") {
-        console.log('You lose!');
-    } else if (computerPick == "paper") {
-        console.log('Your win!');
-    } else if (computerPick == "scissors") {
-        console.log('DRAW');
-    }
+    if (choice == "paper" && computerPicks == "rock") {
+        console.log('You picked Paper and CPU picked Rock: You Win somehow'); 
+        //player ++
+        playerPoints++;
+    }   if (choice == "paper" && computerPicks == "paper") {
+        console.log('You picked paper and CPU picked paper: DRAW');
+    }   if (choice == "paper" && computerPicks == "scissors") {
+        console.log('You picked Paper and CPU picked Scissors: You Lose'); 
+        // computer ++
+        computerPoints++;
+    } 
+    if (choice == "scissors" && computerPicks == "rock") {
+        console.log('You picked Scissors and CPU picked rock: You Lose');
+        // computer ++
+        computerPoints++;
+    }   if (choice == "scissors" && computerPicks == "paper") {
+        console.log('You picked Scissors and CPU picked Paper: You Win'); 
+        // player ++
+        playerPoints++;
+    }   if (choice == "scissors" && computerPicks == "scissors") {
+        console.log('You picked Scissors and CPU picked Scissors: DRAW');
+    } 
+    updateScoreboard();
 }
 
-/*
+
+// Win
+/*function win() {
+    
+}
+
+// Lose
+function lose() {
+    computerPoints++; 
+     
+}
+*/
+// Scoreboard function
+function updateScoreboard() {
+    playerScore.innerHTML = playerPoints;
+    computerScore.innerHTML = computerPoints;
+}
+
+
 // Winning combos 
     //if player picks rock:
         //and comp picks paper
@@ -98,4 +129,3 @@ function playerPick(choice) {
 // best of 3 or first to 2 win
 
 // go again?
-*/
